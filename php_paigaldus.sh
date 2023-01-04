39d4141b866fc4e4d2064b685bae921acc8a8168
Jaana Malm <jaana.malm@voco.ee>
2023-01-04 01:33:04 -0600

    php paigaldamine

---
+++ php_paigaldus.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PHP=$(dpgk-query -W -f='$(Status)' php7.0 2>/dev/null | grep -c 'ok installed')
+if [[ $PHP -eq 0 ]]; then
+	echo "Paigaldame php ja vajalikud lisad"
+	apt install php7.0 libapache2-mod-php7.0 php7.0-mysql
+	echo "php on paigaldatud"
+elif [[ $PHP -eq 1 ]]; then
+	echo "php on juba paigaldatud"
+fi
